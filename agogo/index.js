5d3f400ec16073ec6229548b4b293b208cd9ab11
codeloccol <yonliaoussihabiboulaye@gmail.com>
2023-12-13 19:26:25 +0100

    hi

--- agogo/index.js
+++ agogo/index.js
@@ -1,9 +1,9 @@
-const myTimer = document.querySelector(".timer")
-const boutonUn = document.querySelector(".but1")
-const boutDeux = document.querySelector(".but2")
-const boutTroi = document.querySelector(".but3")
-const bouQuatre =document.querySelector(".but4")
-const boutCinq = document.querySelector(".but5")
+// const myTimer = document.querySelector(".timer")
+// const boutonUn = document.querySelector(".but1")
+// const boutDeux = document.querySelector(".but2")
+// const boutTroi = document.querySelector(".but3")
+// const bouQuatre =document.querySelector(".but4")
+// const boutCinq = document.querySelector(".but5")
 
 
 
@@ -13,94 +13,147 @@ const boutCinq = document.querySelector(".but5")
 
 
 
-let boutUn = 20;
-boutonUn.addEventListener("click",()=>{
-let interval = setInterval(()=>{
-  timer(boutUn)
-  boutUn--
-  if(boutUn===0){
-    clearInterval(interval)
-  }
-},1000)
-});
+// let boutUn = 20;
+// boutonUn.addEventListener("click",()=>{
+// let interval = setInterval(()=>{
+//   timer(boutUn)
+//   boutUn--
+//   if(boutUn===0){
+//     clearInterval(interval)
+//   }
+// },1000)
+// });
 
-let bouDeux = 300;
-boutDeux.addEventListener("click",()=>{
-  let inter = setInterval(()=>{
-    timer(bouDeux)
-    bouDeux--
-    if(bouDeux===0){
-      clearInterval(inter)
-    }
-  },1000)
-});
+// let bouDeux = 300;
+// boutDeux.addEventListener("click",()=>{
+//   let inter = setInterval(()=>{
+//     timer(bouDeux)
+//     bouDeux--
+//     if(bouDeux===0){
+//       clearInterval(inter)
+//     }
+//   },1000)
+// });
 
-let bouTrois = 900;
-boutTroi.addEventListener("click",()=>{
-  let inte = setInterval(()=>{
-    timer(bouTrois)
-    bouTrois--
-    if(bouTrois===0){
-      clearInterval(inte)
-    }
-  },1000)
-});
-
-
+// let bouTrois = 900;
+// boutTroi.addEventListener("click",()=>{
+//   let inte = setInterval(()=>{
+//     timer(bouTrois)
+//     bouTrois--
+//     if(bouTrois===0){
+//       clearInterval(inte)
+//     }
+//   },1000)
+// });
 
 
-let boutquatre = 1200;
-bouQuatre.addEventListener("click",()=>{
-  let int = setInterval(()=>{
-    timer(boutquatre)
-    boutquatre--
-    if(boutquatre===0){
-      clearInterval(int)
-    }
-  },1000)
-})
+
+
+// let boutquatre = 1200;
+// bouQuatre.addEventListener("click",()=>{
+//   let int = setInterval(()=>{
+//     timer(boutquatre)
+//     boutquatre--
+//     if(boutquatre===0){
+//       clearInterval(int)
+//     }
+//   },1000)
+// })
 
 
 
  
 
-let boutCin = 1800;
-boutCinq.addEventListener("click",()=>{
-  let int = setInterval(()=>{
-    timer(boutCin)
-    boutCin--
-    if(boutCin===0){
-      clearInterval(int)
-    }
-  },1000)
-});
+// let boutCin = 1800;
+// boutCinq.addEventListener("click",()=>{
+//   let int = setInterval(()=>{
+//     timer(boutCin)
+//     boutCin--
+//     if(boutCin===0){
+//       clearInterval(int)
+//     }
+//   },1000)
+// });
 
 
-let input = document.getElementById("impTime");
-input.addEventListener("change",(event) => {
-  let input = event.target;
-let value = parseInt(input.value);
-  let ime = setInterval(() => {
-    if (isNaN(value)) {
-      return;
-    }
-    timer(value);
-    value--;
-    if (value===0) {
-      clearInterval(ime);
+// let input = document.getElementById("impTime");
+// input.addEventListener("change",(event) => {
+//   let input = event.target;
+// let value = parseInt(input.value);
+//   let ime = setInterval(() => {
+//     if (isNaN(value)) {
+//       return;
+//     }
+//     timer(value);
+//     value--;
+//     if (value===0) {
+//       clearInterval(ime);
+//     }
+//   }, 1000);
+// });
+
+
+
+
+
+
+// function timer(seconds) {
+//   let heur = Math.floor(seconds/3600)
+//   let minutes = Math.floor(seconds/60%60)
+//   let secondes = Math.floor(seconds%60)
+//   myTimer.innerHTML =`${heur}:${minutes}:${secondes}`
+
+// }
+const myTimer = document.querySelector(".timer");
+const boutonUn = document.querySelector(".but1");
+const boutDeux = document.querySelector(".but2");
+const boutTroi = document.querySelector(".but3");
+const bouQuatre = document.querySelector(".but4");
+const boutCinq = document.querySelector(".but5");
+
+let activeInterval; 
+function startTimer(seconds) {
+  clearInterval(activeInterval); 
+
+  activeInterval = setInterval(() => {
+    timer(seconds);
+    seconds--;
+
+    if (seconds === 0) {
+      clearInterval(activeInterval);
     }
   }, 1000);
+}
+
+boutonUn.addEventListener("click", () => {
+  startTimer(20);
 });
 
+boutDeux.addEventListener("click", () => {
+  startTimer(300);
+});
 
+boutTroi.addEventListener("click", () => {
+  startTimer(900);
+});
 
+bouQuatre.addEventListener("click", () => {
+  startTimer(1200);
+});
 
+boutCinq.addEventListener("click", () => {
+  startTimer(1800);
+});
 
+let input = document.getElementById("impTime");
+input.addEventListener("change", (event) => {
+  let value = parseInt(event.target.value);
+  startTimer(value);
+});
 
 function timer(seconds) {
-  let heur = Math.floor(seconds/3600)
-  let minutes = Math.floor(seconds/60%60)
-  let secondes = Math.floor(seconds%60)
-  myTimer.innerHTML =`${heur}:${minutes}:${secondes}`
-
+  let heures = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds / 60) % 60);
+  let secondes = Math.floor(seconds % 60);
+  myTimer.innerHTML = `${heures}:${minutes}:${secondes}`;
 }
