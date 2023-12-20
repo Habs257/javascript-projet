7117efcdfe86c5cf7d77d197073ae5884ea56e7e
codeloccol <yonliaoussihabiboulaye@gmail.com>
2023-12-20 23:40:04 +0100

    debut

--- calculatrice/index.js
+++ calculatrice/index.js
@@ -1,18 +1,26 @@
 const display = document.querySelector(".display");
 const all = document.querySelectorAll("button");
-const specialChars = ["+", "-", "÷", "*", ".", "="];
+const specialChars = ["*","+","-", "÷", ".", "="];
 let sortie = "";
 
 const calculate = (btnvalue) => {
    if (btnvalue === "=" && sortie !== "") {
       // Utilisez parseFloat pour convertir la chaîne en nombre
-      sortie = eval(sortie.replace("%", "/100"));
+      sortie = eval(sortie.replace("÷", "/"));
    } else if (btnvalue === "RESET") {
       sortie = "";
    } else if (btnvalue === "DEL") {
-      sortie = sortie.toString().slice(0, -1);
+      sortie = sortie.slice(0, -1);
    } else {
       if (sortie === "" && specialChars.includes(btnvalue)) return;
+
+      // Vérifier si le dernier caractère est un opérateur spécial
+      const lastChar = sortie.slice(-1);
+      if (specialChars.includes(lastChar)) {
+         // Remplacer le dernier opérateur spécial s'il y en a déjà un
+         sortie = sortie.slice(0, -1);
+      }
+
       sortie += btnvalue;
    }
    display.value = sortie;
